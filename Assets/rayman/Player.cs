b31528c566768b74c6430852ebda1af08d83ba14
omeranha <53233626+omeranha@users.noreply.github.com>
2024-06-23 23:47:49 -0300

    should be last commit

--- Assets/rayman/Player.cs
+++ Assets/rayman/Player.cs
@@ -8,12 +8,17 @@ public class Player : MonoBehaviour
 	public float speed;
 	public float jumpForce;
 	public int health;
+	public Collider2D attackCollider;
+	public TMPro.TextMeshProUGUI textTMP;
+	public Sprite winGame;
+	public Sprite loseGame;
 	private int jumps;
 	private bool hand;
 	private Rigidbody2D rig;
 	private SpriteRenderer spriteRenderer;
 	Animator anim;
-	public Collider2D attackCollider;
+	private float lastHit;
+	private bool gameFinished;
 	
 	// Start is called before the first frame update
 	void Start()
@@ -28,12 +33,11 @@ public class Player : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if (health == 0) {
-			Destroy(gameObject);
+		if (!gameFinished) {
+			Movement();
+			Punch();
+			CheckEndGame();
 		}
-
-		Movement();
-		Punch();
 	}
 
 	void Movement()
@@ -65,18 +69,49 @@ public class Player : MonoBehaviour
 		}
 	}
 
+	void CheckEndGame()
+	{
+		GameObject[] lums = GameObject.FindGameObjectsWithTag("lum");
+		GameObject[] zombies = GameObject.FindGameObjectsWithTag("zombie");
+
+		if (lums.Length == 0 && zombies.Length == 0) {
+			StopPlayer(winGame);
+		}
+
+		if (health == 0) {
+			StopPlayer(loseGame);
+		}
+	}
+
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		if (collision.gameObject.CompareTag("ground")) {
+		if (collision.gameObject.CompareTag("ground") || collision.gameObject.CompareTag("obstacle")) {
 			jumps = 0;
 			anim.SetBool("jumping", false);
 			anim.SetBool("falling", false);
 		}
 
-		if (collision.gameObject.CompareTag("zombie")) {
-			health--;
-			StartCoroutine(GotHit());
+		if (collision.gameObject.CompareTag("lum")) {
+			textTMP.text = (int.Parse(textTMP.text) + 1).ToString();
+			Destroy(collision.gameObject);
 		}
+	}
+
+	private void OnCollisionStay2D(Collision2D collision) {
+		if (collision.gameObject.CompareTag("zombie") && Time.time - lastHit >= 2f) {
+			if (health > 0) {
+				health--;
+				StartCoroutine(GotHit());
+				lastHit = Time.time;
+			}
+		}
+	}
+
+	void StopPlayer(Sprite sprite)
+	{
+		anim.enabled = false;
+		spriteRenderer.sprite = sprite;
+		gameFinished = true;
 	}
 
 	IEnumerator ActivateAttack()
--- Assets/zombie/Zombie.cs
+++ Assets/zombie/Zombie.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Zombie : MonoBehaviour
 {
 	public int health;
-	public Transform player;
+	public Player player;
 	private Rigidbody2D rig;
 	private SpriteRenderer spriteRenderer;
 	Animator anim;
@@ -17,6 +17,9 @@ public class Zombie : MonoBehaviour
 		rig = GetComponent<Rigidbody2D>();
 		spriteRenderer = GetComponent<SpriteRenderer>();
 		health = 3;
+		for (int i = 1; i < 10; i++) {
+			Physics2D.IgnoreCollision(gameObject.GetComponent<Collider2D>(), GameObject.Find("lum" + i).GetComponent<Collider2D>());
+		}
 	}
 
 	// Update is called once per frame
@@ -26,14 +29,21 @@ public class Zombie : MonoBehaviour
 			Destroy(gameObject);
 		}
 
-		if (!player) {
-			return;
+		Vector2 direction = (player != null) ? player.transform.position - transform.position : Vector2.zero;
+		anim.SetBool("walking", (Mathf.Abs(direction.x) > 1 && rig.velocity.x != 0));
+		if (player.health > 0) {
+			rig.velocity = new Vector2(direction.normalized.x * 2, rig.velocity.y);
+			transform.eulerAngles = (direction.x > 0) ? new Vector3(0, 180, 0) : new Vector3(0, 0, 0);
 		}
 
-		Vector2 direction = player.position - transform.position;
-		rig.velocity = new Vector2(direction.normalized.x * 2, rig.velocity.y);
-		transform.eulerAngles = (direction.x > 0) ? new Vector3(0, 180, 0) : new Vector3(0, 0, 0);
-		anim.SetBool("walking", (Mathf.Abs(direction.x) > 1 && rig.velocity.x != 0));
+		/*
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, 1, -1);
+		if (hit.collider != null) {
+			if (Mathf.Abs(rig.velocity.y) < 0.01f) {
+				rig.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+			}
+		}
+		*/
 	}
 
 	public void Hit() {
